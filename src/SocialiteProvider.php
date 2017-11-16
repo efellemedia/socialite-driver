2b135bcacb04d2c4c2f04bf6b50419b4862cc267
Shea Lewis <shea.lewis89@gmail.com>
2017-11-16 10:49:25 -0800

    Update user object

--- src/SocialiteProvider.php
+++ src/SocialiteProvider.php
@@ -59,7 +59,7 @@ class SocialiteProvider extends AbstractProvider implements ProviderInterface
             'id'         => $user['id'],
             'first_name' => $user['first_name'],
             'last_name'  => $user['last_name'],
-            'fullname'   => $user['first_name'].' '.$user['last_name'],
+            'name'       => $user['first_name'].' '.$user['last_name'],
             'nickname'   => $user['nickname'],
             'email'      => $user['email'],
         ]);
